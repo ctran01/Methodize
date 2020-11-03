ce52f695f41ac93dfd9ecb0787c6f806f4dd6f6c
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-11-03 12:12:38 -0600

    Initial project creation

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+const config = require("./index");
+
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: "postgres",
+  },
+};
