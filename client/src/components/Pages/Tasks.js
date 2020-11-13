88379f05915a61873feb1963e0713c77c8e90154
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-11-12 18:41:32 -0600

    Started home and task page

--- client/src/components/Pages/Tasks.js
+++ client/src/components/Pages/Tasks.js
@@ -1,7 +1,56 @@
 import React from "react";
-
+import TopNavBarTask from "../NavigationBar/TopNavBarTask";
+import "../../css/Task.css";
 const Tasks = () => {
-  return <div>Tasks</div>;
+  return (
+    <>
+      <TopNavBarTask />
+      <div className="tasks-container">
+        <div className="tasks-inner-container">
+          <div className="recently-assigned-tasks-container">
+            <div className="task--header">
+              <h3>Recently Assigned</h3>
+            </div>
+            <ul className="task--list">
+              <li className="task">task 1</li>
+              <li className="task">task 2</li>
+              <li className="task">task 3</li>
+            </ul>
+          </div>
+          <div className="today-tasks-container">
+            <div className="task--header">
+              <h3>Today</h3>
+            </div>
+            <ul className="task--list">
+              <li className="task">task 1</li>
+              <li className="task">task 2</li>
+              <li className="task">task 3</li>
+            </ul>
+          </div>
+          <div className="upcoming-tasks-container">
+            <div className="task--header">
+              <h3>Upcoming</h3>
+            </div>
+            <ul className="task--list">
+              <li className="task">task 1</li>
+              <li className="task">task 2</li>
+              <li className="task">task 3</li>
+            </ul>
+          </div>
+          <div className="later-tasks-container">
+            <div className="task--header">
+              <h3>Later</h3>
+            </div>
+            <ul className="task--list">
+              <li className="task">task 1</li>
+              <li className="task">task 2</li>
+              <li className="task">task 3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Tasks;
