a78c4cb29d03e6b97bfa849ea9bae398111f7933
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-11-21 19:43:54 -0600

    project page styling

--- client/src/components/Pages/Team.js
+++ client/src/components/Pages/Team.js
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import TopNavBar from "../NavigationBar/TopNavBar";
 import apiServer from "../../config/apiServer";
 import Loader from "../Loader";
+import "../../css/TeamPage.css";
+import TeamMemberIcon from "../teams/TeamMemberIcon";
+import ProjectTile from "../projects/ProjectTile";
 
 const TeamPage = () => {
   const { teamId, teamName } = useParams();
@@ -26,10 +29,45 @@ const TeamPage = () => {
     return <Loader />;
   }
 
+  const membersList = () => {
+    return <TeamMemberIcon />;
+  };
+
+  const projectsList = () => {
+    return <ProjectTile />;
+  };
   return (
     <div>
       <TopNavBar name={teamName} />
-      <div>Team Page</div>
+      <div className="team-page-container">
+        <div className="team-page-content-container">
+          <div className="team-page-content-left">
+            <div className="team-content-left-description-container">
+              <div className="team-content-left-description-header">
+                <div className="team-content-title">Description</div>
+              </div>
+              <form className="team-content-left-description-form">
+                <textarea
+                  className="description"
+                  placeholder="Click to add team description..."
+                ></textarea>
+              </form>
+            </div>
+            <div className="team-content-left-members-container">
+              <div className="team-content-left-members-header">
+                <div className="team-content-title">Members</div>
+              </div>
+              <div className="team-content-left-members--list"></div>
+            </div>
+          </div>
+          <div className="team-page-content-right">
+            <div className="team-content-right-header">
+              <div className="team-content-title">Projects</div>
+            </div>
+            <div className="team-content-right-projects--list"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
