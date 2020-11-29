45625a12268a54825a229373505d74babd3be853
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-11-28 22:13:02 -0600

    Add Task form/backend route complete

--- client/src/context/reducer/ProjectReducer.js
+++ client/src/context/reducer/ProjectReducer.js
@@ -2,6 +2,8 @@ const Reducer = (state, action) => {
   switch (action.type) {
     case "get_user_projects":
       return { ...state, projects: action.payload };
+    case "get_project":
+      return { ...state, userProject: action.payload };
     default:
       return state;
   }
---
+++ client/src/context/store/TasklistStore.js
@@ -0,0 +1,18 @@
+import React, { createContext, useReducer } from "react";
+import Reducer from "../reducer/ProjectReducer";
+
+const initialState = {
+  tasklists: [],
+};
+
+const TasklistStore = ({ children }) => {
+  const [tasklistState, tasklistdispatch] = useReducer(Reducer, initialState);
+  return (
+    <Context.Provider value={[tasklistState, tasklistdispatch]}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const Context = createContext(initialState);
+export default TasklistStore;
